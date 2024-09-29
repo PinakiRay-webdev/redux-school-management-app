efb9c3cfa2e72e20804decfa3049563b3c4f8d60
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-09-29 22:48:03 +0530

    implemented localstorage to set then get and then clear it when student performs login and logout activity

--- client/src/pages/Student/StudentHome.jsx
+++ client/src/pages/Student/StudentHome.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const StudentHome = () => {
+
+  const navigate = useNavigate();
+
+  const logout = () =>{
+    localStorage.clear();
+    navigate('/');
+  }
+
   return (
     <div className="w-full h-fit">
       <div className="px-5">
         <p>This is the student home</p>
-        <button>Logout</button>
+        <button onClick={logout} >Logout</button>
       </div>
     </div>
   );
