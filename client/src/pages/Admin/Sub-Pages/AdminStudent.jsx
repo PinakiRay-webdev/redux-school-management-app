5106ae8fb654bb675194a7e5d6fe430cb6b43cb0
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-09-29 20:15:23 +0530

    filtered out students and mentors in their respective oages

--- client/src/pages/Admin/Sub-Pages/AdminStudent.jsx
+++ client/src/pages/Admin/Sub-Pages/AdminStudent.jsx
@@ -3,19 +3,19 @@ import { useSelector , useDispatch } from 'react-redux'
 import { getUsers } from '../../../Redux/slice/userSlice';
 const AdminStudent = () => {
 
-  const usersData = useSelector((state) => state.user)
-
+  const usersData = useSelector((state) => state.user.users.filter(role => role.Role === 'student'))
   const dispatch = useDispatch();
 
   useEffect(()=>{
     dispatch(getUsers())
   },[dispatch])
 
+
   return (
     <div className='w-full h-fit pl-32' >
       <div className='px-3' >
         <h1>This is the student data</h1>
-        {usersData.users.map((Element , id)=>(
+        {usersData.map((Element , id)=>(
           <p key={id} >{Element.FirstName}</p>
         ))}
       </div>
--- client/src/pages/Admin/Sub-Pages/AdminTeacher.jsx
+++ client/src/pages/Admin/Sub-Pages/AdminTeacher.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useSelector , useDispatch } from 'react-redux'
+import { getUsers } from '../../../Redux/slice/userSlice';
 const AdminTeacher = () => {
+
+  const userData = useSelector((state)=> state.user.users.filter(role => role.Role === 'mentor'));
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(getUsers());
+  },[dispatch])
+
   return (
     <div className='w-full h-fit pl-32' >
       <div className='px-3' >
         This is the teacher list
+        {userData.map((Element , id)=>(
+          <p key={id} >{Element.FirstName}</p>
+        ))}
       </div>
     </div>
   )
