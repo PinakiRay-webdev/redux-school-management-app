3802fca13f9a0aae5d6b64cdfd389264524aabea
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-10-08 20:31:29 +0530

    implemented visualizations for student dashboard

--- client/src/pages/Student/sub-pages/StudentDashboard.jsx
+++ client/src/pages/Student/sub-pages/StudentDashboard.jsx
@@ -1,33 +1,73 @@
-import React, { useEffect , useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { getUsers } from '../../../Redux/slice/userSlice';
-import { CircularProgressbar , buildStyles } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
-
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getUsers } from "../../../Redux/slice/userSlice";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import CircularProgress from "@mui/material/CircularProgress";
+import { BarChart } from "@mui/x-charts/BarChart";
 const StudentDashboard = () => {
-  
-  const currentUser = JSON.parse(localStorage.getItem('studentCredentials'));
+  const currentUser = JSON.parse(localStorage.getItem("studentCredentials"));
 
   const studentInfo = useSelector((state) => state.user.students);
-  const dispatch = useDispatch();
-  const [marks, setMarks] = useState(0);
 
+  const currentStudentInfo = studentInfo?.find(
+    (e) => e.Email === currentUser?.student_mail
+  );
 
-  const currentStudentInfo = studentInfo?.find((e) => e.Email === currentUser?.student_mail);
+  const [avgMarks, setAvgMarks] = useState(0);
+  const [marks, setMarks] = useState([]);
 
   useEffect(() => {
-    if(currentStudentInfo){
-      setMarks(currentStudentInfo.Marks)
-    }
-  }, [currentUser]);
+    const delay = setInterval(() => {
+      setAvgMarks(currentStudentInfo?.avgMark);
+      if(currentStudentInfo)
+      {
+        setMarks(Object.values(currentStudentInfo.Marks))
+      }
+    }, 800);
+
+    return () => {
+      clearInterval(delay);
+    };
+  }, [currentStudentInfo]);
+
+
+  
+
 
   return (
-    <div className='pl-32 w-full h-fit'>
-      <div className='px-3 py-4 '>
-        This is the student dashboard
+    <div className="pl-32 w-full h-fit">
+      <div className="px-3 py-4 ">
+        <header className="grid grid-cols-4 gap-4">
+          {/* avg marks section  */}
+          <div className="border border-zinc-300 rounded-lg h-32 px-3 py-2 shadow-xl">
+            <h1 className="font-semibold capitalize">Your Average mark is </h1>
+            <div className="flex items-end gap-3 pt-3">
+              <CircularProgress
+                size={75}
+                variant="determinate"
+                value={avgMarks}
+              />
+              <p className="font-bold text-4xl text-violet-700">
+                {currentStudentInfo?.avgMark}
+              </p>
+            </div>
+          </div>
+        </header>
+
+        <main>
+          <BarChart className="border border-zinc-300 rounded-lg shadow-xl mt-5"
+            xAxis={[
+              { scaleType: "band", data: ["Physics" , "Maths" , "Chemistry" , "Biology" , "IT"] },
+            ]}
+            series={[{data : marks}]}
+            width={1000}
+            height={400}
+          />
+        </main>
       </div>
     </div>
   );
-}
+};
 
 export default StudentDashboard;
