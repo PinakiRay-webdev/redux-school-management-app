1aaa9e7b365a4259589abdd57c9081fa6d7b83ba
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-10-16 10:47:57 +0530

    added a calender

--- client/src/Pages/Global/Calender/Calender.jsx
+++ client/src/Pages/Global/Calender/Calender.jsx
@@ -1,44 +1,28 @@
-import React , {useEffect} from 'react'
-import { useCalendarApp, ScheduleXCalendar } from '@schedule-x/react'
+import { Scheduler } from "@aldabil/react-scheduler";
+import React from 'react'
 import { useSelector } from 'react-redux'
-import {
-  createViewDay,
-  createViewMonthAgenda,
-  createViewMonthGrid,
-  createViewWeek,
-} from '@schedule-x/calendar'
-import { createEventsServicePlugin } from '@schedule-x/events-service'
+const Calender = () => {
 
-import '@schedule-x/theme-default/dist/index.css'
+  const events = [
+    {
+      id : 1,
+      title : "Coffee with anuj",
+      start : new Date('2024-10-17T12:00'),
+      end : new Date('2024-10-17T13:30'),
+      subtitle : 'nfnfn',
+      Localization : 'starbucks',
+    }
+  ]
 
+  const sideBarStatus = useSelector((state) => state.sidebar.isOpen);
 
-function Calender() {
-  const plugins = [createEventsServicePlugin()]
- 
-  const calendar = useCalendarApp({
-    views: [createViewDay(), createViewWeek(), createViewMonthGrid(), createViewMonthAgenda()],
-    events: [
-      {
-        id: '1',
-        title: 'Event 1',
-        start: '2024-10-15',
-        end: '2024-10-16',
-      },
-    ],
-  }, plugins)
-
-  const sideBarStatus = useSelector((state) => state.sidebar.isOpen)
- 
-  useEffect(() => {
-    // get all events
-    calendar.eventsService.getAll()
-  }, [])
- 
   return (
-    <div className={`w-full h-fit overflow-y-hidden ${sideBarStatus ? "pl-[12vw]" : "pl-[5vw]"} transition-all duration-150 ease-in-out`}>
-      <ScheduleXCalendar calendarApp={calendar} />
+    <div className={`w-full h-fit overflow-y-hidden ${sideBarStatus ? "pl-[12vw]" : "pl-[5vw]"} transition-all duration-150 ease-in-out`} >
+      <div className='px-3 py-2' >
+        <Scheduler view="week" events={events} hourFormat="24"/>
+      </div>
     </div>
   )
 }
- 
+
 export default Calender
