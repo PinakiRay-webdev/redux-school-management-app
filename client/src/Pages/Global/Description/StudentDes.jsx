9f572ee57705eb9354d9f68bc951c5747c040666
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-10-22 12:36:54 +0530

    create a student profile for mentor to see each student performance

--- client/src/Pages/Global/Description/StudentDes.jsx
+++ client/src/Pages/Global/Description/StudentDes.jsx
@@ -2,12 +2,18 @@ import React, { useEffect } from 'react'
 import { useSelector , useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom'
 import { getUsers } from '../../../Redux/slice/userSlice';
+import { CiLocationOn, CiHome, CiPhone } from "react-icons/ci";
+import { IoIosFemale, IoIosMale } from "react-icons/io";
+import { BsCake2 } from "react-icons/bs";
+import { FaUserEdit } from "react-icons/fa";
+
 const StudentDes = () => {
 
     const params = useParams();
     const studentID = params.id;
 
     const studentData = useSelector((state) => state.user.students.find((e) => e.id === studentID))
+    const sideBarStatus = useSelector((state) => state.sidebar.isOpen)
     const dispatch = useDispatch();
 
     useEffect(()=>{
@@ -15,9 +21,91 @@ const StudentDes = () => {
     },[dispatch])
 
   return (
-    <div className='w-full h-fit pl-32' >
-        <div className='px-3 py-5' >
-      This is the student of id {studentID} and the name of the student is {studentData.FirstName}
+    <div className={`w-full h-fit ${sideBarStatus ? "pl-[12vw]" : "pl-[5vw]"} `} >
+        <div className='' >
+        <main className="h-[65vh] border relative">
+        <div className="w-full h-[30vh] bg-purple-200">
+            {/* userImage */}
+            <div className="w-[10rem] h-[10rem] rounded-full bg-purple-400 absolute top-32 border-4 border-white  left-8"></div>
+
+            {/* user details  */}
+
+            <div className="absolute bottom-10 left-8 w-[90vw] flex items-start justify-between">
+              <div>
+                <h1 className="capitalize font-bold text-3xl">
+                  {studentData?.FirstName} {studentData?.LastName}
+                </h1>
+                {/* user address  */}
+                <div className="flex gap-2 items-center py-1">
+                  <p>
+                    <CiLocationOn />
+                  </p>
+                  <p>
+                    {studentData?.City ? studentData?.City : "Not Known"},{" "}
+                    {studentData?.State ? studentData?.State : "Not Known"}
+                  </p>
+                </div>
+
+                {/* bio of user  */}
+                <p>
+                  {studentData?.Role} of {studentData?.Class}
+                  <sup>th</sup> class
+                </p>
+
+
+                  <p>
+                    {studentData?.Role} of {studentData?.Department} subject
+                  </p>
+              </div>
+
+              {/* personal details  */}
+              <div>
+                <div className="flex items-center gap-2">
+                  <p>
+                    <CiHome />
+                  </p>
+                  <p>
+                    {studentData?.Landmark
+                      ? studentData?.Landmark
+                      : "Not Known"}
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <p>
+                    <CiPhone />
+                  </p>
+                  <p>
+                    {studentData?.Phone
+                      ? studentData?.Phone
+                      : "Not known"}
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <p>
+                    {studentData?.Gender === "male" ? (
+                      <IoIosMale />
+                    ) : (
+                      <IoIosFemale />
+                    )}
+                  </p>
+                  <p>
+                    {studentData?.Gender
+                      ? studentData?.Gender
+                      : "Not Known"}
+                  </p>
+                </div>
+                <div className="flex items-center gap-2">
+                  <p>
+                    <BsCake2 />
+                  </p>
+                  <p>
+                    {studentData?.DOB ? studentData?.DOB : "Not Known"}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
         </div>
     </div>
   )
