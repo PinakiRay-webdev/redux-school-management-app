7f587b615064effc3c897a31dd176c17fe85e587
PinakiRay-webdev <pinaki.mongodb@gmail.com>
2024-10-22 12:13:41 +0530

    made a weekly timetable for students

--- client/src/Pages/Global/Calender/Calender.jsx
+++ client/src/Pages/Global/Calender/Calender.jsx
@@ -22,19 +22,91 @@ function Calender() {
       createViewMonthAgenda(),
     ],
     events: [
+      // Monday
       {
         id: '1',
-        title: 'Event 1',
-        start: '2024-10-22 10:15',
-        end: '2024-10-22 11:15',
+        title: 'Maths',
+        start: '2024-10-21 07:00',
+        end: '2024-10-21 09:00',
       },
       {
         id: '2',
-        title: 'Event 2',
-        start: '2024-10-23 10:15',
+        title: 'Chemistry',
+        start: '2024-10-21 09:15',
+        end: '2024-10-21 11:15',
+      },
+      // Tuesday
+      {
+        id: '3',
+        title: 'Physics',
+        start: '2024-10-22 07:00',
+        end: '2024-10-22 09:00',
+      },
+      {
+        id: '4',
+        title: 'Biology',
+        start: '2024-10-22 09:15',
+        end: '2024-10-22 11:15',
+      },
+      // Wednesday
+      {
+        id: '5',
+        title: 'IT',
+        start: '2024-10-23 07:00',
+        end: '2024-10-23 09:00',
+      },
+      {
+        id: '6',
+        title: 'Maths',
+        start: '2024-10-23 09:15',
         end: '2024-10-23 11:15',
       },
-    ],
+      // Thursday
+      {
+        id: '7',
+        title: 'Chemistry',
+        start: '2024-10-24 07:00',
+        end: '2024-10-24 09:00',
+      },
+      {
+        id: '8',
+        title: 'Physics',
+        start: '2024-10-24 09:15',
+        end: '2024-10-24 11:15',
+      },
+      // Friday
+      {
+        id: '9',
+        title: 'Biology',
+        start: '2024-10-25 07:00',
+        end: '2024-10-25 09:00',
+      },
+      {
+        id: '10',
+        title: 'IT',
+        start: '2024-10-25 09:15',
+        end: '2024-10-25 11:15',
+      },
+      // Saturday
+      {
+        id: '11',
+        title: 'Maths',
+        start: '2024-10-26 07:00',
+        end: '2024-10-26 08:30',
+      },
+      {
+        id: '12',
+        title: 'Chemistry',
+        start: '2024-10-26 08:45',
+        end: '2024-10-26 10:15',
+      },
+      {
+        id: '13',
+        title: 'Physics',
+        start: '2024-10-26 10:30',
+        end: '2024-10-26 12:00',
+      },
+    ]
   }, plugins)
 
   const sideBarStatus = useSelector((state) => state.sidebar.isOpen)
